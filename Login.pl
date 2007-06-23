dd89460d428cb7dbb13fdf3239c352c3c17757d4
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2007-06-23 19:58:51 +0000

    Update the Login function with the new change in API

--- Login.pl
+++ Login.pl
@@ -1,21 +1,7 @@
+#!/usr/bin/perl -w
+
 use Term::ReadKey;
 use Perlwikipedia;
-
-my $perlwikipedia=Perlwikipedia->new;
-
-print "Please enter the host of the wiki, such as \'en.wikipedia.org\': ";
-
-my $host=<STDIN>;
-
-chomp $host;
-
-print "\nPlease enter the path to index.php, such as \'w\' on Wikipedia: ";
-
-my $path=<STDIN>;
-
-chomp $path;
-
-$perlwikipedia->set_wiki($host,$path);
 
 print "\nPlease enter the username of the account you are logging in: ";
 
@@ -33,8 +19,27 @@ chomp $password;
 
 ReadMode('normal');
 
+my $perlwikipedia=Perlwikipedia->new($username);
+
+# Turn debugging on, to see what the bot is doing
+$perlwikipedia->{debug} = 1;
+
+print "\nPlease enter the host of the wiki, such as \'en.wikipedia.org\': ";
+
+my $host=<STDIN>;
+
+chomp $host;
+
+print "\nPlease enter the path to index.php, such as \'w\' on Wikipedia: ";
+
+my $path=<STDIN>;
+
+chomp $path;
+
+$perlwikipedia->set_wiki($host, $path);
+
 print "\nTrying to log in...";
 
-my $login_status=$perlwikipedia->login($username,$password);
+my $login_status=$perlwikipedia->login($username, $password);
 
 print "\nLogin returned \'$login_status\'\n";
